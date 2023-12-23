250b634df278b12121f44ce947e76c9d984eb7be
Shiben B <shiben@gmail.com>
2023-12-23 23:25:53 +0530

    skeleton of the interface

--- my_py_crypt.py
+++ my_py_crypt.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python3
+
 arg_desc = "File encryption/decryption tool with key/password"
 arg_cmd = "cmd"
 arg_cmd_help = "ek (encrypt with key file) | dk (decrypt with key file) | ep (encrypt with password) | dp (decrypt with password)"
 arg_file = "file"
 arg_file_help = "File to be encrypted or decrypted"
-arg_key = "token"
-arg_key_help = "key file to use if cmd=ek|dk, password to use if cmd=ep|dp. Password will be asked as input if not provided"
+arg_token = "token"
+arg_token_help = "key file to use if cmd=ek|dk, password to use if cmd=ep|dp. Password will be asked as input if not provided"
 
 import argparse
 parser = argparse.ArgumentParser(description=)
 parser.add_argument(arg_cmd, help=arg_cmd_help)
 parser.add_argument(arg_file, help=arg_file_help)
-parser.add_argument(arg_key, help=arg_key_help)
+parser.add_argument(arg_token, help=arg_token_help)
 
 args = parser.parse_args()
 cmd = args.cmd
@@ -19,11 +21,21 @@ token = args.token
 
 del argparse
 
-import mypycryptlib
+import my_py_crypt_lib
 
 if cmd = "ek":
-    create_key(arg_key)
-    encrypt(file, arg_key)
+    print("impl key based enc")
+    #create_key(token)
+    #encrypt(file, token)
+elif cmd = "dk":
+    print("impl key based dec")
+    #key = load_key(token)
+    #decrypt(file, token)
 elif cmd = "ep":
-    
+    print("impl password based enc")
+elif cmd = "dp":
+    print("impl password based dec")
+else
+    print("Invalid Command")
 
+del my_py_crypt_lib
