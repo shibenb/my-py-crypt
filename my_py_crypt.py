00a471f97124061b182083252b30dab9b39aeebb
Shiben B <shiben@gmail.com>
2023-12-24 20:08:55 +0530

    all working fine. file sanity to be done next

--- my_py_crypt.py
+++ my_py_crypt.py
@@ -1,48 +1,65 @@
 #!/usr/bin/python3
 
-arg_desc = "File encryption/decryption tool with key/password"
-arg_cmd = "cmd"
-arg_cmd_help = "ek (encrypt with key file) | dk (decrypt with key file) | ep (encrypt with password) | dp (decrypt with password)"
-arg_file = "file"
-arg_file_help = "File to be encrypted or decrypted"
-arg_token = "token"
-arg_token_help = "key file to use if cmd=ek|dk, password to use if cmd=ep|dp. Password will be asked as input if provided as \"stdin\" (without quotes)"
+arg_desc = "File encryption or decryption tool with key or key + password."
+
+arg_command = "command"
+arg_command_help = "encrypt | decrypt"
+
+arg_infile = "infile"
+arg_infile_help = "Input file to be encrypted or decrypted."
+
+arg_keyfile = "-k"
+arg_keyfile_major = "--keyfile"
+arg_keyfile_help = "Key file to be used for encryption (will be created) or decryption (should be provided) of your input file. If argument not provided, then a file with an added extention of \"key\" will be assumed."
+
+arg_password = "-p"
+arg_password_major = "--password"
+arg_password_help = "Ask for password to accompany the key. If argument not provided, the file will be encrypted or decrypted with only the key. If a file was encrypted with password, then the same passwod must be provided to decrypt it."
+
+arg_outfile = "-o"
+arg_outfile_major = "--outfile"
+arg_outfile_help = "Resultant file of encryption or decryption. If argument not provided, a file with the same name as the input file, with an appropriate added extention, will be created."
 
 import argparse
 parser = argparse.ArgumentParser(description=arg_desc)
-parser.add_argument(arg_cmd, help=arg_cmd_help)
-parser.add_argument(arg_file, help=arg_file_help)
-parser.add_argument(arg_token, help=arg_token_help)
+parser.add_argument(arg_command, help=arg_command_help)
+parser.add_argument(arg_infile, help=arg_infile_help)
+parser.add_argument(arg_keyfile, arg_keyfile_major, help=arg_keyfile_help)
+parser.add_argument(arg_password, arg_password_major, help=arg_password_help, action="store_true")
+parser.add_argument(arg_outfile, arg_outfile_major, help=arg_outfile_help)
 
 args = parser.parse_args()
-cmd = args.cmd
-file = args.file
-token = args.token
+command = args.command
+infile = args.infile
+keyfile = args.keyfile
+password = args.password
+outfile = args.outfile
 
 del argparse
 
 import getpass
 import my_py_crypt_lib
 
-if cmd == "ek":
-    key = my_py_crypt_lib.create_and_load_key(token)
-    my_py_crypt_lib.encrypt(file, key)
-elif cmd == "dk":
-    key = my_py_crypt_lib.load_key(token)
-    my_py_crypt_lib.decrypt(file, key)
-elif cmd == "ep":
-    if token == "stdin":
-        token = getpass.getpass("Password: ")
-        token2 = getpass.getpass("Again: ")
-        if token != token2:
+if command == "encrypt":
+    if password:
+        pass1 = getpass.getpass("Password: ")
+        pass2 = getpass.getpass("Again: ")
+        if pass1 != pass2:
             print("Passwords don't match")
-            exit(1) #return
-    key = my_py_crypt_lib.generate_key(token)
-    my_py_crypt_lib.encrypt(file, key)
-elif cmd == "dp":
-    if token == "stdin":
-        token = getpass.getpass("Password: ")
-    key = my_py_crypt_lib.generate_key(token)
-    my_py_crypt_lib.decrypt(file, key)
+            exit(1)
+        else:
+            key = my_py_crypt_lib.generate_key_and_salt(keyfile, pass1)
+            my_py_crypt_lib.encrypt(infile, key)
+    else:
+        key = my_py_crypt_lib.create_and_load_key(keyfile)
+        my_py_crypt_lib.encrypt(infile, key)
+elif command == "decrypt":
+    if password:
+        pass1 = getpass.getpass("Password: ")
+        key = my_py_crypt_lib.generate_key_with_salt(keyfile, pass1)
+        my_py_crypt_lib.decrypt(infile, key)
+    else:
+        key = my_py_crypt_lib.load_key(keyfile)
+        my_py_crypt_lib.decrypt(infile, key)
 else:
     print("Invalid Command")
--- my_py_crypt_lib.py
+++ my_py_crypt_lib.py
@@ -1,7 +1,7 @@
-from cryptography.fernet import Fernet
+from cryptography import fernet
 
 def create_and_load_key(filename):
-    key = Fernet.generate_key()
+    key = fernet.Fernet.generate_key()
     with open(filename, "wb") as key_file:
         key_file.write(key)
     return key
@@ -10,17 +10,15 @@ def load_key(filename):
     return open(filename, "rb").read()
 
 def encrypt(filename, key):
-    f = Fernet(key)
+    f = fernet.Fernet(key)
     with open(filename, "rb") as file:
         file_data = file.read()
     encrypted_data = f.encrypt(file_data)
     with open(filename, "wb") as file:
         file.write(encrypted_data)
 
-from cryptography import fernet
-
 def decrypt(filename, key):
-    f = Fernet(key)
+    f = fernet.Fernet(key)
     with open(filename, "rb") as file:
         encrypted_data = file.read()
     try:
@@ -35,17 +33,17 @@ from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 import secrets
 import base64
 
-import os
-
-def generate_key(password, salt_size = 16, length = 32, n = 16384, r = 8, p = 1):
-    salt_size = 16
-    salt = secrets.token_bytes(salt_size)
-    if(os.path.exists("salt.salt")):
-        salt = open("salt.salt", "rb").read()
-    else:
-        with open("salt.salt", "wb") as file:
-            file.write(salt)
-
+def generate_key(salt, password, length = 32, n = 16384, r = 8, p = 1):
     kdf = Scrypt(salt=salt, length=length, n=n, r=r, p=p)
     derived_key = kdf.derive(password.encode())
     return base64.urlsafe_b64encode(derived_key)
+
+def generate_key_with_salt(saltfile, password):
+    salt = open(saltfile, "rb").read()
+    return generate_key(salt, password)
+
+def generate_key_and_salt(saltfile, password, salt_size = 16):
+    salt = secrets.token_bytes(salt_size)
+    with open(saltfile, "wb") as file:
+        file.write(salt)
+    return generate_key(salt, password)
