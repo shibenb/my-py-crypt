67a20e6cda8227b3ec8d22e0264ee59d4307c267
devrat <devrat@devbox>
2023-12-25 14:21:22 +0530

    fixed arguments text, and unnecessary removal of args parser

--- my_py_crypt.py
+++ my_py_crypt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-arg_desc = "File encryption or decryption tool with key or key + password."
+arg_desc = "File encryption or decryption tool with key or along with a password."
 
 arg_command = "command"
 arg_command_help = "encrypt | decrypt"
@@ -10,15 +10,15 @@ arg_infile_help = "Input file to be encrypted or decrypted."
 
 arg_keyfile = "-k"
 arg_keyfile_major = "--keyfile"
-arg_keyfile_help = "Key file to be used for encryption (will be created) or decryption (should be provided) of your input file. If argument not provided, then a file with an added extention of \"key\" will be assumed."
+arg_keyfile_help = "Key file with this name will be created in case of encryption. An existing key file is required for decryption. If argument not provided, then a file with the same name as infile, with an added appropriate extention, will be assumed as an argument."
 
 arg_password = "-p"
 arg_password_major = "--password"
-arg_password_help = "Ask for password to accompany the key. If argument not provided, the file will be encrypted or decrypted with only the key. If a file was encrypted with password, then the same passwod must be provided to decrypt it."
+arg_password_help = "Will ask to create a password to pair with the key (named \"salt\") in case of encryption. Will ask an existing password is required for decryption. If argument not provided, the file will be encrypted or decrypted with only the keyfile. If a file was encrypted with password, then the same passwod must be provided to decrypt it."
 
 arg_outfile = "-o"
 arg_outfile_major = "--outfile"
-arg_outfile_help = "Resultant file of encryption or decryption. If argument not provided, a file with the same name as the input file, with an appropriate added extention, will be created."
+arg_outfile_help = "Resultant file of encryption or decryption. If argument not provided, then a file with the same name as infile, with an added appropriate added extention, will be created."
 
 import argparse
 parser = argparse.ArgumentParser(description=arg_desc)
@@ -29,37 +29,30 @@ parser.add_argument(arg_password, arg_password_major, help=arg_password_help, ac
 parser.add_argument(arg_outfile, arg_outfile_major, help=arg_outfile_help)
 
 args = parser.parse_args()
-command = args.command
-infile = args.infile
-keyfile = args.keyfile
-password = args.password
-outfile = args.outfile
-
-del argparse
 
 import getpass
 import my_py_crypt_lib
 
-if command == "encrypt":
-    if password:
+if args.command == "encrypt":
+    if args.password:
         pass1 = getpass.getpass("Password: ")
         pass2 = getpass.getpass("Again: ")
         if pass1 != pass2:
             print("Passwords don't match")
             exit(1)
         else:
-            key = my_py_crypt_lib.generate_key_and_salt(keyfile, pass1)
-            my_py_crypt_lib.encrypt(infile, key)
+            key = my_py_crypt_lib.generate_key_and_salt(args.keyfile, pass1)
+            my_py_crypt_lib.encrypt(args.infile, key)
     else:
-        key = my_py_crypt_lib.create_and_load_key(keyfile)
-        my_py_crypt_lib.encrypt(infile, key)
-elif command == "decrypt":
-    if password:
+        key = my_py_crypt_lib.create_and_load_key(args.keyfile)
+        my_py_crypt_lib.encrypt(args.infile, key)
+elif args.command == "decrypt":
+    if args.password:
         pass1 = getpass.getpass("Password: ")
-        key = my_py_crypt_lib.generate_key_with_salt(keyfile, pass1)
-        my_py_crypt_lib.decrypt(infile, key)
+        key = my_py_crypt_lib.generate_key_with_salt(args.keyfile, pass1)
+        my_py_crypt_lib.decrypt(args.infile, key)
     else:
-        key = my_py_crypt_lib.load_key(keyfile)
-        my_py_crypt_lib.decrypt(infile, key)
+        key = my_py_crypt_lib.load_key(args.keyfile)
+        my_py_crypt_lib.decrypt(args.infile, key)
 else:
     print("Invalid Command")
